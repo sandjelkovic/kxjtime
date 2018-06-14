ba1d81c005cbff9e0188bae67d8ff9c32b4dbb9f
stefan <sa.stefan.andjelkovic@gmail.com>
2018-06-14 12:59:01 +0200

    Extend duration to work with ZonedDateTime, Instant and LocalDateTime via infix functions

--- src/main/kotlin/com/sandjelkovic/kxjtime/DurationExtensions.kt
+++ src/main/kotlin/com/sandjelkovic/kxjtime/DurationExtensions.kt
@@ -1,9 +1,26 @@
 package com.sandjelkovic.kxjtime
 
 import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 /**
  * @author sandjelkovic
  * @date 14.6.18.
  */
 operator fun Duration.unaryMinus(): Duration = this.negated()
+
+infix fun Duration.before(time: ZonedDateTime): ZonedDateTime = time.minus(this)
+
+infix fun Duration.after(time: ZonedDateTime): ZonedDateTime = time.plus(this)
+
+infix fun Duration.before(instant: Instant): Instant = instant.minus(this)
+
+infix fun Duration.after(instant: Instant): Instant = instant.plus(this)
+
+infix fun Duration.before(time: LocalDateTime): LocalDateTime = time.minus(this)
+
+infix fun Duration.after(time: LocalDateTime): LocalDateTime = time.plus(this)
+
+infix fun Duration.and(other: Duration): Duration = this.plus(other)
--- src/test/kotlin/com.sandjelkovic/kxjtime/DurationExtensionsTest.kt
+++ src/test/kotlin/com.sandjelkovic/kxjtime/DurationExtensionsTest.kt
@@ -1,14 +1,19 @@
 package com.sandjelkovic.kxjtime
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 /**
  * @author sandjelkovic
  * @date 14.6.18.
  */
 internal class DurationExtensionsTest {
+    val fiveMinutes = Duration.ofMinutes(5)!!
+    val fiveHours = Duration.ofHours(5)!!
 
     @Test
     fun `Should negate positive duration via - operator`() {
@@ -16,7 +21,7 @@ internal class DurationExtensionsTest {
 
         val negatedDuration = -tenMinutes
 
-        Assertions.assertTrue(negatedDuration == Duration.ofMinutes(-10))
+        assertTrue(negatedDuration == Duration.ofMinutes(-10))
     }
 
     @Test
@@ -25,6 +30,84 @@ internal class DurationExtensionsTest {
 
         val negatedDuration = -minusTenMinutes
 
-        Assertions.assertTrue(negatedDuration == Duration.ofMinutes(10))
+        assertTrue(negatedDuration == Duration.ofMinutes(10))
     }
+
+    @Test
+    fun `Should combine durations with an infix function`() {
+        val twoHours: Duration = Duration.ofHours(2)
+        val thirtyMinutes: Duration = Duration.ofMinutes(30)
+
+        val combinedDuration = twoHours and thirtyMinutes
+
+        assertTrue(combinedDuration == Duration.ofHours(2).plus(Duration.ofMinutes(30)))
+    }
+
+    @Test
+    fun `Should combine more than two durations via infix function chaining`() {
+        val twoHours: Duration = Duration.ofHours(2)
+        val thirtyMinutes: Duration = Duration.ofMinutes(30)
+        val tenMinutes: Duration = Duration.ofMinutes(10)
+        val fiveSeconds: Duration = Duration.ofSeconds(5)
+
+        val combinedDuration = twoHours and thirtyMinutes and fiveSeconds and tenMinutes and twoHours
+
+        val desiredDuration = Duration.ofHours(4).plusMinutes(40).plusSeconds(5)
+        assertTrue(combinedDuration == desiredDuration)
+    }
+
+    @Test
+    fun `Should use infix before by subtracting Duration from ZonedDateTime`() {
+        val zonedDateTime = ZonedDateTime.now()!!
+
+        val calculatedTime = fiveMinutes before zonedDateTime
+
+        assertTrue(calculatedTime == zonedDateTime.minusMinutes(5))
+    }
+
+    @Test
+    fun `Should use infix after by adding Duration to ZonedDateTime`() {
+        val zonedDateTime = ZonedDateTime.now()!!
+
+        val calculatedTime = fiveHours after zonedDateTime
+
+        assertTrue(calculatedTime == zonedDateTime.plusHours(5))
+    }
+
+    @Test
+    fun `Should use infix before by subtracting Duration from LocalDateTime`() {
+        val localDateTime = LocalDateTime.now()!!
+
+        val calculatedTime = fiveMinutes before localDateTime
+
+        assertTrue(calculatedTime == localDateTime.minusMinutes(5))
+    }
+
+    @Test
+    fun `Should use infix after by adding Duration to LocalDateTime`() {
+        val localDateTime = LocalDateTime.now()!!
+
+        val calculatedTime = fiveHours after localDateTime
+
+        assertTrue(calculatedTime == localDateTime.plusHours(5))
+    }
+
+    @Test
+    fun `Should use infix before by subtracting Duration from an Instant`() {
+        val instant = Instant.now()!!
+
+        val calculatedTime = fiveMinutes before instant
+
+        assertTrue(calculatedTime == instant.minusSeconds(5 * 60))
+    }
+
+    @Test
+    fun `Should use infix after by adding Duration to an Instant`() {
+        val instant = Instant.now()!!
+
+        val calculatedTime = fiveHours after instant
+
+        assertTrue(calculatedTime == instant.plusSeconds(5 * 60 * 60))
+    }
+
 }

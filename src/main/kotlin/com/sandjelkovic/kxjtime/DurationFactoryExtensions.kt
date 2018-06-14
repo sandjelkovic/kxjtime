85a46bf388842a19ffcc9f280194faa80b019a82
stefan <sa.stefan.andjelkovic@gmail.com>
2018-06-14 11:05:03 +0200

    Add support to create Durations from Long numbers

--- src/main/kotlin/com/sandjelkovic/kxjtime/DurationFactoryExtensions.kt
+++ src/main/kotlin/com/sandjelkovic/kxjtime/DurationFactoryExtensions.kt
@@ -19,3 +19,21 @@ inline val Int.hours: Duration
 
 inline val Int.days: Duration
     get() = Duration.ofDays(toLong())
+
+inline val Long.nanoseconds: Duration
+    get() = Duration.ofNanos(this)
+
+inline val Long.milliseconds: Duration
+    get() = Duration.ofMillis(this)
+
+inline val Long.seconds: Duration
+    get() = Duration.ofSeconds(this)
+
+inline val Long.minutes: Duration
+    get() = Duration.ofMinutes(this)
+
+inline val Long.hours: Duration
+    get() = Duration.ofHours(this)
+
+inline val Long.days: Duration
+    get() = Duration.ofDays(this)
--- src/test/kotlin/com.sandjelkovic/kxjtime/DurationFactoryExtensionsTest.kt
+++ src/test/kotlin/com.sandjelkovic/kxjtime/DurationFactoryExtensionsTest.kt
@@ -8,8 +8,9 @@ import java.time.Duration
  * @author sandjelkovic
  * @date 13.6.18.
  */
-internal class TimeExtensionsTest {
+internal class DurationFactoryExtensionsTest {
     private val integerNumber: Int = 5
+    private val longNumber: Long = 15
 
     @Test
     fun `Should create nanoseconds Duration from integer`() {
@@ -56,6 +57,55 @@ internal class TimeExtensionsTest {
     @Test
     fun `Should create 10 years of Duration via days from integer`() {
         val duration = (365 * 10).days
+
+        assertTrue(duration == Duration.ofDays((365 * 10)))
+    }
+
+    @Test
+    fun `Should create nanoseconds Duration from long`() {
+        val duration = longNumber.nanoseconds
+
+        assertTrue(duration == Duration.ofNanos(longNumber))
+    }
+
+    @Test
+    fun `Should create miliseconds Duration from long`() {
+        val duration = longNumber.milliseconds
+
+        assertTrue(duration == Duration.ofMillis(longNumber))
+    }
+
+    @Test
+    fun `Should create seconds Duration from long`() {
+        val duration = longNumber.seconds
+
+        assertTrue(duration == Duration.ofSeconds(longNumber))
+    }
+
+    @Test
+    fun `Should create nanoseconds duration from long`() {
+        val duration = longNumber.minutes
+
+        assertTrue(duration == Duration.ofMinutes(longNumber))
+    }
+
+    @Test
+    fun `Should create hours Duration from long`() {
+        val duration = longNumber.hours
+
+        assertTrue(duration == Duration.ofHours(longNumber))
+    }
+
+    @Test
+    fun `Should create days Duration from long`() {
+        val duration = longNumber.days
+
+        assertTrue(duration == Duration.ofDays(longNumber))
+    }
+
+    @Test
+    fun `Should create 10 years of Duration via days from long`() {
+        val duration = (365 * 10).toLong().days
 
         assertTrue(duration == Duration.ofDays((365 * 10)))
     }

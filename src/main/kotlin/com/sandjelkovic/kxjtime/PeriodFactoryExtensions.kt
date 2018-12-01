69b102079d2be48b5e9abe9526300b43d2e1b52c
StefanAndjelkovic <sa.stefan.andjelkovic@gmail.com>
2018-12-01 11:50:53 +0100

    Update Period factories documentation

--- src/main/kotlin/com/sandjelkovic/kxjtime/PeriodFactoryExtensions.kt
+++ src/main/kotlin/com/sandjelkovic/kxjtime/PeriodFactoryExtensions.kt
@@ -2,12 +2,23 @@ package com.sandjelkovic.kxjtime
 
 import java.time.Period
 
-
+/**
+ * Create a Period from receiver value handled as weeks
+ * @return Period
+ */
 inline val Int.weeks: Period
     get() = Period.ofWeeks(this)!!
 
+/**
+ * Create a Period from receiver value handled as months
+ * @return Period
+ */
 inline val Int.months: Period
     get() = Period.ofMonths(this)!!
 
+/**
+ * Create a Period from receiver value handled as years
+ * @return Period
+ */
 inline val Int.years: Period
     get() = Period.ofYears(this)!!
